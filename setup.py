a18cfac2ea247eb220a94981ee7040731ffee52b
terapyon <Manabu TERADA>
2025-04-03 21:10:08 +0900

    Bump version to 0.2.0 with additional styling features

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="net_vis",
-    version="0.1.1",
+    version="0.2.0",
     description="NetVis is a package for interactive visualization Python NetworkX graphs within Jupyter Lab",
     packages=find_packages(),
     install_requires=[
