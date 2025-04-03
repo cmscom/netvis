1e55dbe537bfe8faf40d703e60f9212898062cb0
terapyon <Manabu TERADA>
2025-04-03 20:59:55 +0900

    Fix package installation and skip notebook tests in CI

--- setup.py
+++ setup.py
@@ -1,2 +1,29 @@
-# setup.py shim for use with applications that require it.
-__import__("setuptools").setup()
+from setuptools import setup, find_packages
+
+setup(
+    name="net_vis",
+    version="0.1.1",
+    description="NetVis is a package for interactive visualization Python NetworkX graphs within Jupyter Lab",
+    packages=find_packages(),
+    install_requires=[
+        "ipywidgets>=8.0.0",
+    ],
+    extras_require={
+        "docs": [
+            "jupyter_sphinx",
+            "nbsphinx",
+            "nbsphinx-link",
+            "pypandoc",
+            "pytest_check_links",
+            "recommonmark",
+            "sphinx>=1.5",
+            "sphinx_rtd_theme",
+        ],
+        "examples": [],
+        "test": [
+            "nbval",
+            "pytest-cov",
+            "pytest>=6.0",
+        ],
+    }
+)
