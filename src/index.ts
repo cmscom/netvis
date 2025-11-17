815d104f1e4faf5f376e058f5bfe0bef64ef20b7
terapyon <Manabu TERADA>
2025-11-17 14:12:38 +0900

    bugfix for js

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Manabu TERADA
 // Distributed under the terms of the Modified BSD License.
 
-// Export plugin as default for JupyterLab to load
-export { default } from './plugin';
+// Export MIME extension as default for JupyterLab to load
+export { default } from './mime';
 
 // Export other modules for backward compatibility
 export * from './version';
